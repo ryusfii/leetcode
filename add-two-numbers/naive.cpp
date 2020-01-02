bed0f3dd1bd1046bc8734a515d8cfb3387527ecf
ryusfii <59442031+ryusfii@users.noreply.github.com>
2020-01-02 15:23:51 +0100

    add-two-numbers: Naive c++ implementation

---
+++ add-two-numbers/naive.cpp
@@ -0,0 +1,108 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode* i1 = l1;
+        ListNode* i2 = l2;
+        ListNode* res = new ListNode(0);
+        ListNode* r = res;
+        ListNode* zero = new ListNode(0);
+        
+        while (true) {
+            r->val = (i1->val + i2->val + carry);
+            if (r->val > 9) {
+                carry = r->val / 10;
+                r->val = r->val % 10;
+            } else {
+                carry = 0;
+            }
+
+            if (i1->next == nullptr && i2->next == nullptr && carry == 0) break;
+            if (i1->next == nullptr) i1 = zero; else i1 = i1->next;
+            if (i2->next == nullptr) i2 = zero; else i2 = i2->next;
+            r->next = new ListNode(0);
+            r = r->next;
+        }
+        
+        return res;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string listNodeToString(ListNode* node) {
+    if (node == nullptr) {
+        return "[]";
+    }
+
+    string result;
+    while (node) {
+        result += to_string(node->val) + ", ";
+        node = node->next;
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* l1 = stringToListNode(line);
+        getline(cin, line);
+        ListNode* l2 = stringToListNode(line);
+        
+        ListNode* ret = Solution().addTwoNumbers(l1, l2);
+
+        string out = listNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
